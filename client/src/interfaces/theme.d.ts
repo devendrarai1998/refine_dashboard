20aeabe8072d5fd2f38f892103bc85e1c1144efe
Devendra Kumar Rai <68963294+devendrarai1998@users.noreply.github.com>
2023-03-23 23:54:47 +0530

    first commit

---
+++ client/src/interfaces/theme.d.ts
@@ -0,0 +1,18 @@
+/*eslint-disable */
+import '@pankod/refine-mui';
+
+export interface CustomTheme {
+  // Add custom variables here like below:
+  // status: {
+  //   danger: string;
+  // };
+}
+
+declare module '@pankod/refine-mui' {
+  interface Theme extends import('@pankod/refine-mui').Theme, CustomTheme {}
+  interface ThemeOptions
+    extends import('@pankod/refine-mui').ThemeOptions,
+      CustomTheme {}
+}
+
+/*eslint-enable */
